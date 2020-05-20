aef990ddb4bdb42d0990eb5700303ec40ae36879
Gaharana <mr.priyanshgaharana@gmail.com>
2020-05-20 14:51:45 +0530

    changed sevice

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const apiUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://linkedin-oauth-api-dot-tutorial-262713.el.r.appspot.com/'
+    ? 'https://linkedin-oauth-api-dot-tutorial-262713.el.r.appspot.com'
     : 'http://localhost:4000';
 const config = {
   apiUrl,
